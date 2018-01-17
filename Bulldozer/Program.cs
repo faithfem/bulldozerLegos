515c6b928090cec029b68fe7680765d9c531444f
faithfem <fmugure@intracitygeeks.org>
2018-01-17 01:33:47 -0500

    creating classes

---
+++ Bulldozer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulldozer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new BullDozer();
+            dozer.color = "Yellow";
+            dozer.size = "Large";
+
+            Console.WriteLine("Bulldozer is: " + dozer.color);
+        }
+    }
+
+    public class BullDozer
+    {
+
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
