310f95102d18196555f6ac63c4b215746caabc01
faithfem <fmugure@intracitygeeks.org>
2018-01-17 21:30:32 -0500

    Sec2Lec6 Create Class and Object

--- Bulldozer/Program.cs
+++ Bulldozer/Program.cs
@@ -10,17 +10,21 @@ namespace Bulldozer
     {
         static void Main(string[] args)
         {
-            var dozer = new BullDozer();
-            dozer.color = "Yellow";
-            dozer.size = "Large";
+            var dozer = new BullDozer ("Yellow", "Mini" );
+            var dozer2 = new BullDozer ( "Red", "Large" );        
 
             Console.WriteLine("Bulldozer is: " + dozer.color);
+            Console.WriteLine("Bulldozer2 is: " + dozer2.color + " and " + dozer2.size);
         }
     }
 
     public class BullDozer
     {
-
+        public BullDozer(string _color, string _size) //constructor
+        {
+            this.color = _color;
+            this.size = _size;
+        }
         public string color { get; set; }
         public string size { get; set; }
     }
