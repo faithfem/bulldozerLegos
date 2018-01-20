f50b180cae7c454d1153b435d613104ba1e3a8f2
faithfem <fmugure@intracitygeeks.org>
2018-01-20 14:45:38 -0500

    inheritance

--- Man.cs
+++ Man.cs
@@ -8,36 +8,52 @@ namespace Bulldozer
         {
             var man = new Man();
             var kart = new Kart();
-            var alien = new Alien();
+            var alien = new Alien("Purple");
+            alien.color();
+
+            Console.WriteLine("Alien is " + alien.color);
         }
     }
 
+    //moves and color are common amongst man, kart & alien
+    public class GeneralObject //objectThatMovesSizeColor
+    {
+        //enter constructor (has same name as class)
+        public GeneralObject() { 
+        }
+          //give constructor the common properties ur objects have
+          //then delete those properties from the objects
+          public bool moves { get; set; }
+          public string color { get; set; }
+        }
 
 
-public class Man
-{
-    public Man() {
+    public class Man: GeneralObject
+    {
+        public Man()
+        {
+        }
+        //public bool moves { get; set; } //moves & color common denominator
+        public string size { get; set; }
+        //public string color { get; set; } //moves & color common denominator
     }
-    public bool moves { get; set; }
-    public string size { get; set; }
-    public string color { get; set; }
-}
-public class Kart
-{
-    public Kart(){
+    public class Kart: GeneralObject
+    {
+        public Kart() {
+        }
+        //public bool moves { get; set; } after constructor is created, removed all moves & color properties from all objects
+        public string manufacturer { get; set; }
+        //public string color { get; set; }
     }
-    public bool moves { get; set; }
-    public string manufacturer { get; set; }
-    public string color { get; set; }
-}
 
-public class Alien
-{
-    public Alien(){
+    public class Alien: GeneralObject
+    {
+        public Alien() {
+        }
+
+        //public bool moves { get; set; }
+        public string planet { get; set; }
+        //public string color { get; set; }
     }
-   
-    public bool moves { get; set; }
-    public string planet { get; set; }
-    public string color { get; set; }
 }
 
